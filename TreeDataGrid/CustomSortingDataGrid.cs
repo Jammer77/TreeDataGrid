04904b13321b219c30e66641dfe9ebb0f91036bb
nickolay-sadovoy <nickolay.sadovoy@gmail.com>
2017-08-31 17:11:37 +0300

    Add Sorting

---
+++ TreeDataGrid/CustomSortingDataGrid.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Controls;
+using static TreeDataGrid.MainWindow;
+
+namespace TreeDataGrid
+{
+    public class CustomSortingDataGrid : DataGrid
+    {
+        protected override void OnSorting(DataGridSortingEventArgs eventArgs)
+        {
+            var ListTestData = ItemsSource as List<TestData>;
+
+            if (eventArgs.Column.CanUserSort && ListTestData != null)
+            {
+                if (!string.IsNullOrEmpty(eventArgs.Column.SortMemberPath))
+                {
+                    var rootLevelItems = ListTestData.Where(rootTestData => ListTestData.Any(testData => testData.Children != null && !testData.Children.Contains(rootTestData)));
+
+                    IEnumerable<TestData> sortedList = null;
+                    var currentColumn = this.Columns.FirstOrDefault(col => col == eventArgs.Column);
+                    var newDirection = ListSortDirection.Descending;
+                    
+                    if (eventArgs.Column.SortDirection == ListSortDirection.Descending )
+                    {
+                        newDirection = ListSortDirection.Ascending;
+                        sortedList = rootLevelItems.OrderBy(o => o.GetType().GetProperty(eventArgs.Column.SortMemberPath).GetValue(o, null));
+
+                    }
+                    else
+                    {
+                        newDirection = ListSortDirection.Descending;
+                        sortedList = rootLevelItems.OrderByDescending(o => o.GetType().GetProperty(eventArgs.Column.SortMemberPath).GetValue(o, null));
+                    }
+
+                    this.ItemsSource = ListHelper.GetList(sortedList);
+
+                    if (currentColumn != null)
+                        currentColumn.SortDirection = newDirection;
+                }
+            }
+            else
+            {
+                base.OnSorting(eventArgs);
+            }
+        }
+    }
+}
--- TreeDataGrid/MainWindow.xaml.cs
+++ TreeDataGrid/MainWindow.xaml.cs
@@ -1,60 +1,79 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TreeDataGrid
 {
-	public class TestData
-	{
-		public string Name { get; set; }
-		public string Description => "Test";
-		public IEnumerable<TestData> Children{get;set;}
-	}
+    public class TestData : INotifyPropertyChanged
+    {
+        private string name;
+        private string description = "Test";
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; NotifyPropertyChanged("Name"); }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; NotifyPropertyChanged("Description"); }
+        }
+
+        public IEnumerable<TestData> Children { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
 
 	public partial class MainWindow : Window
 	{
+		private List<TestData> _testDataCollection;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 		}
 
-		private void Button_Click(object sender, RoutedEventArgs e)
-		{
-			var testDataCollection = new List<TestData>();
-			for (int i = 0; i < 10; i++)
-			{
-				var newItem = new TestData()
-				{
-					Name = "Parent"
-				};
-				var children = new List<TestData>();
-				for (int j = 0; j < 10; j++)
-				{
-					var child = new TestData()
-					{
-						Name = "Child"
-					};
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _testDataCollection = new List<TestData>();
+            for (int i = 0; i < 10; i++)
+            {
+                var newItem = new TestData()
+                {
+                    Name = "Parent",
+                    Description = i.ToString()
 
-					if (j == 0)
-					{
-						child.Children = new List<TestData>()
-						{
-							new TestData()
-							{
-								Name = "SubChild"
-							},
+                };
+                var children = new List<TestData>();
+                for (int j = 0; j < 10; j++)
+                {
+                    var child = new TestData()
+                    {
+                        Name = "Child",
+                        Description = i.ToString() + j.ToString()
+                    };
+
+                    if (j == 0)
+                    {
+                        child.Children = new List<TestData>()
+                        {
+                            new TestData()
+                            {
+                                Name = "SubChild"
+                            },
 
 							new TestData()
 							{
@@ -65,32 +84,53 @@ namespace TreeDataGrid
 								Name = "SubChild"
 							},
 						};
-						SelectConverter.SelectedItems.Add(child);
+		//				SelectConverter.SelectedItems.Add(child);
 					}
 					
 					children.Add(child);
 				}
 				newItem.Children = children;
-				testDataCollection.Add(newItem);
+				_testDataCollection.Add(newItem);
 			}
-			SelectConverter.SelectedItems.Add(testDataCollection.FirstOrDefault());
-			dataGrid.ItemsSource = GetList(testDataCollection);
+	//		SelectConverter.SelectedItems.Add(_testDataCollection.FirstOrDefault());
+			dataGrid.ItemsSource = ListHelper.GetList(_testDataCollection);
 		}
 
-		private IEnumerable<TestData> GetList(IEnumerable<TestData> testDataCollection)
-		{
-			var result = new List<TestData>();
+        public static class ListHelper
+        {
+            public static IEnumerable<TestData> GetList(IEnumerable<TestData> testDataCollection)
+            {
+                var result = new List<TestData>();
 
-			foreach (var item in testDataCollection)
+                foreach (var item in testDataCollection)
+                {
+                    result.Add(item);
+                    if (item.Children != null && SelectConverter.SelectedItems.Any(i => i == item))
+                    {
+                        var allchildren = GetList(item.Children);
+                        result.AddRange(allchildren);
+                    }
+                }
+                return result;
+            }
+        }
+
+		private void Button_Click_1(object sender, RoutedEventArgs e)
+		{
+			var testData = (sender as CheckBox).DataContext as TestData;
+			if (SelectConverter.SelectedItems.Any(o => o == testData))
 			{
-				result.Add(item);
-				if (item.Children != null && SelectConverter.SelectedItems.Any(i => i == item))
-				{
-					var allchildren = GetList(item.Children);
-					result.AddRange(allchildren);
-				}
+				SelectConverter.SelectedItems.Remove(testData);
+                dataGrid.ItemsSource = ListHelper.GetList(_testDataCollection);
+            }
+			else
+			{
+				SelectConverter.SelectedItems.Add(testData);
+                var newValues = dataGrid.ItemsSource as List<TestData>;
+                if(newValues!=null)
+                    dataGrid.ItemsSource = ListHelper.GetList(newValues);
 			}
-			return result;
+            
 		}
 	}
 }
